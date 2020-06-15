6c641ac8c1beeb96cbb3beabbb85d65726657242
Matheus <matheus.nogueira3@fatec.sp.gov.br>
2020-06-15 17:00:35 -0300

    Front-end

---
+++ frontend/build/precache-manifest.1f1a7a072bb3fc56a9ab07316fd612f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc17f00032ee874465bca98fa10dd820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47fbe0575d764076bc38",
+    "url": "/static/css/main.50bd68c0.chunk.css"
+  },
+  {
+    "revision": "f476f0b63f7c49c464b3",
+    "url": "/static/js/2.96b2f2ef.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.96b2f2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fbe0575d764076bc38",
+    "url": "/static/js/main.3baaba4b.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
